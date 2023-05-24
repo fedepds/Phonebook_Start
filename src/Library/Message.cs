e65f2800a6f876c64c72ae142c3bc29c1e0bfeb7
fedepds <federico.pizarro@correo.ucu.edu.uy>
2023-05-24 20:58:50 -0300

    Update Message.cs

--- src/Library/Message.cs
+++ src/Library/Message.cs
@@ -1,11 +1,21 @@
 namespace PhoneBook
 {
+<<<<<<< Updated upstream
     public class Message 
+=======
+    public class Message  : IMessageChannel
+>>>>>>> Stashed changes
     {
-        protected Message(string from, string to)
+        protected Message(string from, string to, string text)
         {
             this.From = from;
             this.To = to;
+            this.Text = text;
+        }
+
+        public void Send( Message message)
+        {
+
         }
 
 
